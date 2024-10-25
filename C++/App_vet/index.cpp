e035cb292c0c8b77a5413c006afcbd93d18e935e
blaballos <laballosbrandon@gmail.com>
2024-10-25 13:36:41 -0300

    Fix in show slient function

--- C++/App_vet/index.cpp
+++ C++/App_vet/index.cpp
@@ -3,8 +3,6 @@
 
 using std::cin, std::cout, std::vector, std::string;
 
-vector<Client> client_list{};
-
 class Client {
     private:
         string name;
@@ -53,23 +51,39 @@ class Client {
         }
 };
 
+vector<Client> client_list{};
+void register_client(Client client);
+void show_client();
+
+int main() {
+    // TEST
+
+    // Client first_client("Brandon","Laballos","laballosbrandon@gmail.com", 1122334455);
+    // Client second_client("Jeremias","Perez","jereperez@gmail.com", 1133445566);
+
+    // register_client(first_client);
+    // register_client(second_client);
+    show_client();
+}
+
 void register_client(Client client) {
     client_list.push_back(client);
     string name = client.getName();
     string last_name = client.getLastName();
-    cout << name << last_name << "fue registrado correctamente.";
+    cout << name << " " << last_name << " " << "fue registrado correctamente." << "\n";
 }
 
 void show_client() {
-    for (auto client : client_list) {
-        string name = client.getName();
-        string last_name = client.getLastName();
-        string email = client.getEmail();
+    if (client_list.size() == 0) {
+        cout << "No client registered" << "\n";
+    } else {
+        cout << "Client list: " << "\n";
+        for (auto client : client_list) {
+            string name = client.getName();
+            string last_name = client.getLastName();
+            string email = client.getEmail();
 
-        cout << name << last_name << email << "\n";
+            cout << name << " " << last_name << " " << email << "\n";
+        }
     }
-}
-
-int main() {
-
 }
